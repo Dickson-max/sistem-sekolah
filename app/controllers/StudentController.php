baf485c6ac50df5153806fd217ddcfd15b219984
Dickson-max <dicksonsiip@gmail.com>
2026-01-27 13:14:14 +0700

    wip: implement student controller

---
+++ app/controllers/StudentController.php
@@ -0,0 +1,26 @@
+<?php
+namespace App\Controllers;
+
+
+
+class StudentController
+{
+
+    public function index()
+    {
+        echo '<H1>Daftar Siswa</H1>';
+        echo '<p>Menampilkan daftar siswa</p>';
+    }
+
+    public function create()
+    {
+        echo '<H1>Daftar Siswa</H1>';
+        echo '<p>Menampilkan form tambah siswa</p>';
+    }
+
+}
+
+
+
+
+?>
--- app/core/Router.php
+++ app/core/Router.php
@@ -7,8 +7,21 @@ class Router
 
     public function run()
     {
-        
+        $method = $_SERVER['REQUEST_METHOD'];
+        $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+        if ($method === 'GET' && $uri == '/students') {
+           require_once './app/controllers/StudentController.php';
+            return;
+            }
+          if ($method === 'GET' && $uri == '/students/create') {
+            
+            return;
+            }
+            
     }
+
+        
 
 
 }
