7bd1292fc8e9c3834a7e0bae1e74cc03238a8639
Dickson-max <dicksonsiip@gmail.com>
2026-04-09 13:53:00 +0700

    feat: implement layout

---
+++ app/views/layouts/partials/header.php
@@ -0,0 +1,5 @@
+<div class="p-4 shadow rounded-lg bg-white">
+        <h1 class="text-2xl font-bold">Daftar Siswa</h1>
+        <p>Berikut adalah daftar siswa yang terdaftar di sistem.</p>
+    </div>
+
--- app/views/students/create.php
+++ app/views/students/create.php
@@ -1,24 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tambah Siswa</title>
-    <link rel="stylesheet" href="/css/output.css">
-</head>
-
-<body class="min-h-screen flex flex-col bg-gray-100">
-    <!-- Start -->
-    <header class="bg-red-500 text-white">
-    <div class="flex items-center justify-between container mx-auto p-4">
-        <a href="/students" class="font-bold text-xl"> Sistem Sekolah</a>
-        <a href="/students/create" class="bg-white text-blue-500 px-4 py-2 rounded-lg hover:bg-blue-200">+ Tambah Siswa</a>
-    </div>
-    </header>
-    <!-- End -->
-
-<!-- Main Start -->
-<main class="container mx-auto grow">
 <div class="mt-8 space-y-2">
     <!-- Card Header Start -->
     <div class="p-4 shadow rounded-lg bg-white">
@@ -54,16 +33,3 @@
 </div>
     <!-- Card Body End -->
 </div>
-</main>
-<!-- Main End -->
-
-    <!-- Start -->
-<footer class="bg-gray-800 text-white">
-    <div class="text-center p-4">
-        &copy <?= date('Y')?> Sistem Sekolah - SMK Kristen Immanuel
-    </div>
-</footer>
-    <!--  End -->
-</body>
-
-</html>
