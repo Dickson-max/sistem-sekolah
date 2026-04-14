ca669d6a93f6a72c0a2a76a4f77d6e6b57ef13f3
Dickson-max <dicksonsiip@gmail.com>
2026-04-14 13:15:55 +0700

    feat: implement student index page b

--- app/controllers/StudentController.php
+++ app/controllers/StudentController.php
@@ -1,16 +1,22 @@
 <?php
 namespace App\Controllers;
 require_once '../app/core/Controller.php';
+require_once '../app/models/Student.php';
 
 use App\Core\Controller;
-
+use App\Models\Student;
 
 class StudentController extends Controller
 {
 
     public function index()
     {
-        $this->view('students.index');
+        $studentModel = new Student();
+        $students = $studentModel->getStudents();
+
+        $this->view('students.index', [
+            'students' => $students
+        ]);
     }
 
     public function create()
@@ -25,7 +31,7 @@ class StudentController extends Controller
 
     public function edit(string $id)
     {
-       $this->view('students.edit');
+        $this->view('students.edit');
     }
 
 
--- app/views/students/index.php
+++ app/views/students/index.php
@@ -20,21 +20,34 @@
             </tr>
         </thead>
         <tbody>
-            <tr>
-                <td class="px-4 py-2 text-left">1</td>
-                <td class="px-4 py-2 text-left">Andi</td>
-                <td class="px-4 py-2 text-left">1234</td>
-                <td class="px-4 py-2 text-left">XI TKJ 2</td>
-                <td class="px-4 py-2 text-left">081234567891</td>
+            <?php foreach($students as $index => $student): ?>
+<tr>
+                <td class="px-4 py-2 text-left">
+                    <?= $index + 1?>
+                </td>
+                <td class="px-4 py-2 text-left">
+                    <?= $student['name'] ?>
+                </td>
+                <td class="px-4 py-2 text-left">
+                    <?= $student['nis']?>
+                </td>
+                <td class="px-4 py-2 text-left">
+                    <?= $student['class'] ?>
+                </td>
+                <td class="px-4 py-2 text-left">
+                    <?= $student['phone_number']?>
+                </td>
                 <td class="px-4 py-2">
                    <DIV class="flex justify-center items-center gap-4">
-                    <a href="/students/1" class="text-green-500">Detail</a>
-                    <a href="/students/1/edit" class="text-yellow-500">Edit</a>
+                    <a href="/students/<?= $student['id'] ?>" class="text-green-500">Detail</a>
+                    <a href="/students/<?= $student['id'] ?>/edit" class="text-yellow-500">Edit</a>
                     <a href="" class="text-red-500">Hapus</a>
 
                    </DIV>
                 </td>
             </tr>
+            <?php endforeach?>
+            
         </tbody>
     </table>
 </div>
